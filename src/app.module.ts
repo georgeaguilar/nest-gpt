3626f5a693af4c03e95711f0c27811cd1b07e3fd
Jorge Aguilar <georgeaguilar11@hotmail.com>
2026-01-28 21:35:53 -0600

    feat: gtp service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { GptModule } from './gpt/gpt.module';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [ConfigModule.forRoot(), GptModule],
+})
 export class AppModule {}
